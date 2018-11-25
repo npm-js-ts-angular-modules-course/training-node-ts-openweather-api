358333aadf3319cee656682067b1cb15207bf06b
Anartz Mugika <mugan86@gmail.com>
2018-11-25 20:20:57 +0100

    Export values from services

--- index.ts
+++ index.ts
@@ -1,9 +1,1 @@
-import { CitiesService } from './lib/classes/services/cities.service';
-import { ApiService } from './lib/classes/services/api.service';
-
-const api = new ApiService('ec32f42ea9357dae4e8e8dbc6d0f77f9');
-api.getCurrentWeatherInSelectCityByName('').then( value =>
-    console.log(value)
-);
-
-
+export * from './lib/classes/services/services';
