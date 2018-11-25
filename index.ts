d30280cc676cb9ae409c80c3b3b0a7465d75b43c
Anartz Mugika <mugan86@gmail.com>
2018-11-25 17:33:21 +0100

    Apply changes

--- index.ts
+++ index.ts
@@ -3,6 +3,6 @@ import { ApiService } from './classes/services/api.service';
 export * from './classes/services/api.service';
 
 const api = new ApiService('58dda22dc16b4ec458a95a0a7f2e921d');
-api.getCurrentWeatherInLondon().then( data => {
-    console.log(data)
-});
+api.getCurrentWeatherInLondon().then( value =>
+    console.log(value)
+);
