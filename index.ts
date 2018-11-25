566abac4378232a90011485c5a9613d645d0d092
Anartz Mugika <mugan86@gmail.com>
2018-11-25 19:43:39 +0100

    Update

--- index.ts
+++ index.ts
@@ -1,8 +1,15 @@
+import { CitiesService } from './lib/classes/services/cities.service';
 import { ApiService } from './lib/classes/services/api.service';
 
-export * from './lib/classes/services/api.service';
-
 const api = new ApiService('ec32f42ea9357dae4e8e8dbc6d0f77f9');
-api.getCurrentWeatherInLondon().then( value =>
+api.getCurrentWeatherInSelectCityByName('Roma').then( value =>
     console.log(value)
 );
+
+const cities = new CitiesService;
+console.log((cities.getList()));
+
+cities.printCitiesList();
+
+console.log(cities.selectCity('Barcelona'));
+
