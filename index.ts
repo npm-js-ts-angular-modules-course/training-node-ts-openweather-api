6b8af0774a46d844eec33b05baaef8f20562255e
Anartz Mugika <mugan86@gmail.com>
2018-11-25 08:37:37 +0100

    Call to get correct current weather data

--- index.ts
+++ index.ts
@@ -3,4 +3,6 @@ import { ApiService } from './classes/services/api.service';
 export * from './classes/services/api.service';
 
 const api = new ApiService('58dda22dc16b4ec458a95a0a7f2e921d');
-console.log(api.getCurrentWeatherInLondon);
+api.getCurrentWeatherInLondon().then( data => {
+    console.log(data)
+});
