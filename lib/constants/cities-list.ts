757ef8f5833d0bb8bddc73917d42cb102c619c05
Anartz Mugika <mugan86@gmail.com>
2018-11-26 08:21:10 +0100

    Add cities constant values

---
+++ lib/constants/cities-list.ts
@@ -0,0 +1,43 @@
+import { ListData } from './../classes/interfaces/list.interface';
+
+export const CITIES_LIST: ListData = {
+    "list": [
+        {
+            "name": "Barcelona",
+            "country": "es",
+            "location": {
+                "lat": 41.40340260589794    ,      
+                "lng": 2.186880111694336
+            },
+            "zip": "08026"
+        },
+        {
+            "name": "Bilbao",
+            "country": "es",
+            "location": {
+                "lat": 43.26    ,    
+                "lng": -2.94
+            },
+            "zip": "48002"
+        },
+        {
+            "name": "Paris",
+            "country": "fr",
+            "location": {
+                "lat": 48.856614    ,    
+                "lng": 2.3522219000000177
+            },
+            "zip": "75006"
+        },
+        {
+            "name": "Roma",
+            "country": "it",
+            "location": {
+                "lat": 41.9027835    ,    
+                "lng": 12.496365500000024
+            },
+            "zip": 333
+        }
+    ],
+    "last_update": "2018-11-25"
+};
