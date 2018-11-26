a685baaebf9b5421efc52527712a4f5007c7a2e5
Anartz Mugika <mugan86@gmail.com>
2018-11-26 08:51:01 +0100

    Add all examples with Las Vegas

--- check.ts
+++ check.ts
@@ -1,8 +1,22 @@
-import { Result } from './lib/classes/interfaces/openweather.interface';
+import { Result } from './lib/classes/interfaces/api.interface';
 import { ApiService } from './lib/classes/services/api.service';
 
 
 const api = new ApiService('ec32f42ea9357dae4e8e8dbc6d0f77f9');
-api.getCurrentWeatherInSelectCityByName('Roma').then( (value: Result) =>
-    console.log(value.weather)
+api.getCurrentWeatherByCity('Las Vegas').then( (value: Result) => {
+        console.log('***********By Name ****************');
+        console.log(value.weather);
+    }
+);
+
+api.getCurrentWeatherByLocation({lat: 36.1699412, lng: -115.13982959999998}).then( (value: Result) => {
+        console.log('***********By Location ****************');
+        console.log(value);
+    }
+);
+
+api.getCurrentWeatherByZip('89104').then( (value: Result) => {
+        console.log('***********By Zip ****************');
+        console.log(value);
+    }   
 );
