d1c1ced728ced727390cef56a3e9664fe43afb0b
Anartz Mugika <mugan86@gmail.com>
2018-11-26 08:21:26 +0100

    Take cities list from constants

--- lib/classes/services/cities.service.ts
+++ lib/classes/services/cities.service.ts
@@ -1,46 +1,6 @@
+import { CITIES_LIST } from './../../constants/cities-list';
 export class CitiesService {
-    list: any = {
-        "list": [
-            {
-                "name": "Barcelona",
-                "country": "es",
-                "location": {
-                    "lat": 41.40340260589794    ,      
-                    "lng": 2.186880111694336
-                },
-                "zip": "08026"
-            },
-            {
-                "name": "Bilbao",
-                "country": "es",
-                "location": {
-                    "lat": 43.26    ,    
-                    "lng": -2.94
-                },
-                "zip": "48002"
-            },
-            {
-                "name": "Paris",
-                "country": "fr",
-                "location": {
-                    "lat": 48.856614    ,    
-                    "lng": 2.3522219000000177
-                },
-                "zip": "75006"
-            },
-            {
-                "name": "Roma",
-                "country": "it",
-                "location": {
-                    "lat": 41.9027835    ,    
-                    "lng": 12.496365500000024
-                },
-                "zip": 333
-            }
-        ],
-        "last_update": "2018-11-25"
-    };
-
+    list = CITIES_LIST;
     /**
      * Take available cities list
      * @param json Return in JSON or no. Default "true"
