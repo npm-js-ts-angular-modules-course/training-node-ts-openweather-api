000a8d113d4143a7aa66e2362fb0ec83912fa46c
Anartz Mugika <mugan86@gmail.com>
2018-11-26 08:20:35 +0100

    Value convert any type to result type

--- check.ts
+++ check.ts
@@ -1,7 +1,8 @@
+import { Result } from './lib/classes/interfaces/openweather.interface';
 import { ApiService } from './lib/classes/services/api.service';
 
 
 const api = new ApiService('ec32f42ea9357dae4e8e8dbc6d0f77f9');
-api.getCurrentWeatherInSelectCityByName('').then( value =>
-    console.log(value)
+api.getCurrentWeatherInSelectCityByName('Roma').then( (value: Result) =>
+    console.log(value.weather)
 );
