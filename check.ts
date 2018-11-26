17c9face19540aea1aeb3eff20ccac82911415c1
Anartz Mugika <mugan86@gmail.com>
2018-11-26 09:31:16 +0100

    Take error value and add new check options

--- check.ts
+++ check.ts
@@ -2,21 +2,42 @@ import { Result } from './lib/classes/interfaces/api.interface';
 import { ApiService } from './lib/classes/services/api.service';
 
 
-const api = new ApiService('ec32f42ea9357dae4e8e8dbc6d0f77f9');
-api.getCurrentWeatherByCity('Las Vegas').then( (value: Result) => {
-        console.log('***********By Name ****************');
-        console.log(value.weather);
-    }
+const api = new ApiService('ec32f42ea9357dae4e8e8dbc6d0f77f9', 'm', 'es');
+api.getCurrentWeatherByCity('Madrid', 'uk').then( (value: Result) => {
+    console.log('***********By Name ****************');
+    console.log(value);
+}
+).catch(error => {
+console.log('***********By Name ****************');
+console.log(error.response.body);
+}
 );
 
+api.getCurrentWeatherByCity('Barcelona', 'es').then( (value: Result) => {
+    console.log('***********By Name ****************');
+    console.log(value);
+}
+).catch(error => {
+console.log('***********By Name ****************');
+console.log(error.response.body);
+}
+);
+
+
 api.getCurrentWeatherByLocation({lat: 36.1699412, lng: -115.13982959999998}).then( (value: Result) => {
-        console.log('***********By Location ****************');
-        console.log(value);
-    }
+    console.log('***********By Location ****************');
+    console.log(value);
+}
+).catch(error => {
+    console.log(error.response.body);
+}
 );
 
 api.getCurrentWeatherByZip('89104').then( (value: Result) => {
-        console.log('***********By Zip ****************');
-        console.log(value);
-    }   
+    console.log('***********By Zip ****************');
+    console.log(value);
+}
+).catch(error => {
+    console.log(error.response.body);
+}
 );
--- lib/classes/services/api.service.ts
+++ lib/classes/services/api.service.ts
@@ -1,18 +1,17 @@
 import { Location } from './../interfaces/location.interface';
-import { CitiesService } from './cities.service';
 import { RequestService } from './request.service';
 import * as request from 'request-promise-native';
 
 export class ApiService {
     private API_KEY: string;
     private req: RequestService;
-    private cities: CitiesService;
     private unitMetric: string;
-    constructor(apiKey: string, unitMetric: string = 'm') {
+    private language: string;
+    constructor(apiKey: string, unitMetric: string = 'm', language: string = 'en') {
         this.API_KEY = apiKey;
         this.req = new RequestService();
-        this.cities = new CitiesService();
         this.unitMetric = this.defineUnitMetric(unitMetric);
+        this.language = `&lang=${language}`;
     }
 
     /**
@@ -26,36 +25,51 @@ export class ApiService {
         return ``;
     }
     /**
-     * 
-     * @param city 
+     * Take current weather of select city and country.
+     * @example
+     * getCurrentWeatherByCity('Madrid', 'es') --> Madrid (Spain) current weather Data.
+     * getCurrentWeatherByCity('Barcelona', '', true) --> Find Barcelona (Spain)
+     * @param city { string} add select city.
+     * @param country { string } add country code value.
      */
-    getCurrentWeatherByCity(city: string) {
+    getCurrentWeatherByCity(city: string = '', country: string = '') {
         // Si queremos mapear 
         /*return request.get(options).then( value => {
                 return value['coord']
             }
         );*/
         let findValue = '';
-        if (city === null || city === undefined || city === '') {
+        if ((city === null || city === undefined || city === '')
+                && country === null || country === undefined || country === '') {
             findValue = 'London,uk';
         } else {
-            const c = this.cities.selectCity(city);
-            if (c === undefined) {
-                findValue = 'q=London,uk';
-            } else {
-                findValue = `q=${ c.name },${ c.country}`;
-            }
+                findValue = `q=${ city },${ country }`;
         }
 
-        return request.get(this.req.getQuery(`weather?${ findValue }${ this.unitMetric }&appid=${ this.API_KEY }`));
+        return request.get(this.req.getQuery(`weather?${ findValue }${ this.unitMetric }${ this.language }&appid=${ this.API_KEY }`));
     }
 
+    /**
+     * Take current weather data in select place to add coordinates
+     * @example 
+     * location = {lat: 36.1699412, lng: -115.13982959999998} - Las Vegas
+     * location = {lat: 41.9027835, lng: 12.496365500000024} - Roma
+     * @param location {Location} Add location coordinates in {lat: number, lng: number } format
+     */
     getCurrentWeatherByLocation(location: Location) {
         
         const value = `lat=${ location.lat }&lon=${ location.lng }`;
         return request.get(this.req.getQuery(`weather?${ value }${ this.unitMetric }&appid=${ this.API_KEY }`));
     }
 
+    /**
+     * Input select city zip code to take this select weather current data.
+     * @example
+     * Las Vegas = 48104
+     * Barcelona = 08026
+     * Bilbao = 48002
+     * @param zipCode { string } location zip code add. 
+     */
     getCurrentWeatherByZip(zipCode: string = "-1") {
         return request.get(this.req.getQuery(`weather?zip=${ zipCode }${ this.unitMetric }&appid=${ this.API_KEY }`));
 
