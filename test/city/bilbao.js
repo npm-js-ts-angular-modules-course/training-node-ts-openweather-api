10f292e6a60c8f6ffb643c25000f325ecab59bae
Anartz Mugika <mugan86@gmail.com>
2018-11-27 21:11:32 +0100

    Add Bilbao request in English

---
+++ test/city/bilbao.js
@@ -0,0 +1,31 @@
+module.exports =
+    {
+        coord: { lon: -2.94, lat: 43.26 },
+        weather:
+            [{ id: 800, main: 'Clear', description: 'clear sky', icon: '01n' }],
+        base: 'stations',
+        main:
+        {
+            temp: 9.36,
+            pressure: 1021,
+            humidity: 76,
+            temp_min: 8,
+            temp_max: 11
+        },
+        visibility: 10000,
+        wind: { speed: 2.1, deg: 130 },
+        clouds: { all: 0 },
+        dt: 1543347000,
+        sys:
+        {
+            type: 1,
+            id: 6395,
+            message: 0.0026,
+            country: 'ES',
+            sunrise: 1543303218,
+            sunset: 1543336720
+        },
+        id: 3128026,
+        name: 'Bilbao',
+        cod: 200
+    };
