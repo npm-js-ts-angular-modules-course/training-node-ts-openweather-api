5dbb6b173b12654bbc7a68ee4cf5f3db75471f6c
Anartz Mugika <mugan86@gmail.com>
2018-11-27 20:33:53 +0100

    Update test

--- test/index.js
+++ test/index.js
@@ -14,22 +14,24 @@ const query = {
     'lang': 'es',
     'appid': 'ec32f42ea9357dae4e8e8dbc6d0f77f9'
 };
+const apiEndPoint = 'https://api.openweathermap.org/';
+const pathWithoutQuery = '/data/2.5/weather';
 
-describe('Get User tests', () => {
-    beforeEach(() => {
-        nock('https://api.openweathermap.org/')
-            .log(console.log)
-            .get('/data/2.5/weather')
+describe('Zip is 89104 that corresponding to "Las Vegas, UUSS"', async () => {
+    beforeEach(async () => {
+        nock(apiEndPoint)
+            /*.log(console.log)*/
+            .get(pathWithoutQuery)
             .query(query)
             .reply(200, lasVegasZip);
     });
-    it('Get a user by username', () => {
+    it('Check returning data is correct', async () => {
         const api = new apiService('ec32f42ea9357dae4e8e8dbc6d0f77f9', 'm', 'es');
-
-        console.log(lasVegasZip);
-        const value = api.getCurrentWeather('zip', ['89104', true]);
-        value.subscribe(data => {
-            expect(typeof lasVegasZip).equal.to('object');
-        })
+        
+        return api.getCurrentWeather('zip', ['89104', true]).subscribe(result => {
+            expect(typeof result).equal.to('object');
+            // expect(lasVegasZip.response.request.path).equal.to(`${pathWithoutQuery}?zip=${query.zip}&units=${query.units}&lang=${query.lang}&appid=${query.appid}`);
+            done();
+        });
     });
 });
